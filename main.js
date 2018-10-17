bfd559290aeb86b1703887af0d40e21354f2f182
SeaCrap <506076395@qq.com>
2018-10-17 21:11:16 +0800

    DOM事件

---
+++ main.js
@@ -0,0 +1,27 @@
+//window.onload=function(){
+//  clickMe.addEventListener('click',(e)=>{
+//    popover.style.display = 'block'
+//    console.log('出现')
+//  })
+//
+//  wrapper.addEventListener('click',(e)=>{
+//    e.stopPropagation()
+//    console.log('阻断')
+//  })
+//
+//  document.addEventListener('click',(e)=>{
+//    popover.style.display = 'none'
+//    console.log('隐藏')
+//  })   
+//}
+window.onload = function(){
+  $(clickMe).on('click',(e)=>{
+    $(popover).show()
+    setTimeout(function(){
+      $(document).one('click',()=>{
+        $(popover).hide()
+      })
+    },0)
+  })  
+}
+

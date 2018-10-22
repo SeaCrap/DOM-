6018f78c8f28386be41ef9963f459b1eca25e0c8
SeaCrap <506076395@qq.com>
2018-10-22 21:15:31 +0800

    toggle()

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-window.onload=function(){
-  clickMe.addEventListener('click',(e)=>{
-    popover.style.display = 'block'
-    console.log('出现')
-  })
-  wrapper.addEventListener('click',(e)=>{
-    e.stopPropagation()
-    console.log('阻断')
-  })
-  document.addEventListener('click',(e)=>{
-    popover.style.display = 'none'
-    console.log('隐藏')
-  })   
-}
-//  $(clickMe).on('click',(e)=>{
-//    $(popover).show()
-//    setTimeout(function(){
-//      $(document).one('click',()=>{
-//        $(popover).hide()
-//      })
-//    },0)
-//  })  
-//
+//window.onload=function(){
+//  clickMe.addEventListener('click',(e)=>{
+//    popover.style.display = 'block'
+//    console.log('出现')
+//  })
+//  wrapper.addEventListener('click',(e)=>{
+//    e.stopPropagation()
+//    console.log('阻断')
+//  })
+//  document.addEventListener('click',(e)=>{
+//    popover.style.display = 'none'
+//    console.log('隐藏')
+//  })   
+//}
+  $(clickMe).on('click',(e)=>{
+    $(popover).toggle()
+    setTimeout(function(){
+      $(document).one('click',()=>{
+        $(popover).hide()
+      })
+    },0)
+  })  
+
 

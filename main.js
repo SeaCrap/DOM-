e10d34009182708d0d35567e9ece64659457ee9e
SeaCrap <506076395@qq.com>
2018-10-22 20:21:40 +0800

    End

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-//window.onload=function(){
-//  clickMe.addEventListener('click',(e)=>{
-//    popover.style.display = 'block'
-//    console.log('出现')
-//  })
+window.onload=function(){
+  clickMe.addEventListener('click',(e)=>{
+    popover.style.display = 'block'
+    console.log('出现')
+  })
+  wrapper.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    console.log('阻断')
+  })
+  document.addEventListener('click',(e)=>{
+    popover.style.display = 'none'
+    console.log('隐藏')
+  })   
+}
+//  $(clickMe).on('click',(e)=>{
+//    $(popover).show()
+//    setTimeout(function(){
+//      $(document).one('click',()=>{
+//        $(popover).hide()
+//      })
+//    },0)
+//  })  
 //
-//  wrapper.addEventListener('click',(e)=>{
-//    e.stopPropagation()
-//    console.log('阻断')
-//  })
-//
-//  document.addEventListener('click',(e)=>{
-//    popover.style.display = 'none'
-//    console.log('隐藏')
-//  })   
-//}
-  $(clickMe).on('click',(e)=>{
-    $(popover).show()
-    setTimeout(function(){
-      $(document).one('click',()=>{
-        $(popover).hide()
-      })
-    },0)
-  })  
-
 
